8371881d4006d3e1afcbdda3b648d882a8f05780
yegao <familyink@163.com>
2018-06-06 11:59:23 +0800

    test 1.0.4

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const { getOptions } = require('loader-utils');
-const utils = require('./utils.js')
+const utils = require('./utils.js');
+
 module.exports = function(source) {
   const options = getOptions(this);
+  console.log(options);
   return utils.replace(source,options);
 }
